8692733f077decf14999f9d911ef43300c2861f4
elias.carballo <elias.carballo@mmtdigital.co.uk>
2022-05-10 13:41:44 +0100

    update config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,12 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['https://www.vodafone.co.uk/mobile/phones/pay-monthly-contracts'],
+    },
     upload: {
       target: 'lhci',
-      serverBaseUrl: 'https://powerful-basin-31641.herokuapp.com/app/projects',
-      token: 'Your *build token* goes here', // could also use LHCI_TOKEN variable instead
+      serverBaseUrl: 'https://powerful-basin-31641.herokuapp.com',
+      token: '468d4287-b920-4933-8739-103155ff895d', // could also use LHCI_TOKEN variable instead
     },
   },
 };
